7329ca51b0bc916f8aeee6a394a23fd89763c083
Lu Jia-Qing <4B330110@stust.edu.tw>
2026-09-17 07:49:33 +0000

    add humid

--- hello.py
+++ hello.py
@@ -5,8 +5,12 @@ import time
 print("Hello world")
 print("Node OS:", platform.system(), platform.machine())
 
-for i in range(5):
+for i in range(6):
     temp = round(random.uniform(24.0, 32.0), 1)  # 模擬溫度
     status = "ALERT" if temp > 30 else "ok"
     print(f"[{i}] temp = {temp} C  {status}")
     time.sleep(1)
+
+
+h = round(random.uniform(40.0, 80.0), 1)  # 模擬溫度
+print(f"h = {h} C  {status}")
